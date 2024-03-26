29e1890bfbdb9f880123d495f117a17fc8c118c2
Teodora <tedi.slava@yahoo.com>
2024-03-26 15:47:18 +0200

    stamp probe limited to 4 phototransistors

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ ads = ADS.ADS1115(i2c)
 # Define the analog input channel
 transistor_channels = [AnalogIn(ads, ADS.P0), AnalogIn(ads, ADS.P1), AnalogIn(ads, ADS.P2), AnalogIn(ads, ADS.P3)]
 
-#channel = AnalogIn(ads, ADS.P0)
+channel = AnalogIn(ads, ADS.P0)
 treshhold = 28650
 
 baudrate = 24000000
@@ -34,11 +34,15 @@ pins = [cs_pin, dc_pin, reset_pin]
 
 
 button_pin = 26
+adc_vdd = 21
+GPIO.setup(adc_vdd, GPIO.OUT)
+GPIO.output(adc_vdd, GPIO.HIGH)
 GPIO.setup(button_pin, GPIO.IN)
 
 stamp_arr = []
 time_str = "0:0"
 index_tran = [0]
+is_finished_flags = [False]
 
 
 
@@ -52,18 +56,28 @@ if disp._disp_setting.rotation % 180 == 90:
 # Display image
 
 def read_transistors():
- 
-    current_trace_key = transistor_channels[index_tran[0]]
-    current_value = current_trace_key.value
+    result = 0
+    
+    try:
+        current_trace_key = transistor_channels[index_tran[0]]
+        current_value = current_trace_key.value
+        result = current_value
 
-    print("INDEX ")
-    print(index_tran[0])
-    print(current_value)
+        print("INDEX ")
+        print(index_tran[0])
+        print(current_value)
 
-    if(current_value < treshhold):
-        index_tran[0] += 1
 
-    return current_value
+        if(current_value < treshhold):
+            index_tran[0] += 1
+
+        
+    except :
+        print("Error occured with indeces - probe finished")
+        is_finished_flags.append(True)
+        pass
+
+    return result
  
 
 def tick():
@@ -77,10 +91,11 @@ def tick():
     #tick timer
     while True:
         #clean flags, initiate anew
-        
+      
         flags.clear()
         flags.append(False)
 
+       
         tran_value = read_transistors()
 
         if (flags_transistors[len(flags_transistors) - 1]): 
@@ -93,12 +108,13 @@ def tick():
         counter_arr[1] = str(next_counter)
         counter_text = ":".join(counter_arr)
 
-        if(tran_value < treshhold):
-            flags_transistors.append(True)
-            stamp_arr.append(str(next_counter))
-            
+        if(is_finished_flags[len(is_finished_flags) - 1] == False):
+            if(tran_value < treshhold):
+                flags_transistors.append(True)
+                stamp_arr.append(str(next_counter))
+                
              
-
+        sleep(1)
         #print(stamp_arr)
     
 
@@ -107,8 +123,8 @@ def main():
     disp._disp_setting.image(start_screen(fontsize))
 
     while True:
-        button_state = GPIO.input(button_pin) 
-        if( button_state == 0 ):
-            tick()
+      button_state = GPIO.input(button_pin) 
+      if( button_state == 0 ):
+        tick()
 
 main()
